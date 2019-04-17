7479a951bbc8b08699976047230e9a2da1f1f0f7
Ferhat Yildiz <ferhat.yildiz@turingts.com>
2019-04-17 10:56:38 +0200

    updated bootstrapper

--- scripts/centos/setup-cli.sh
+++ scripts/centos/setup-cli.sh
@@ -3,7 +3,7 @@
 yum -y install git
 yum -y install ansible
 
-cd /home/ansible && \
+cd /home/ansible/terraform-azure-playground/ansible && \
 export GIT_SSL_NO_VERIFY=true && \
 ansible-galaxy install -p roles -r requirements.yml && \
 ansible-playbook -i inventories/local playbook.yml
