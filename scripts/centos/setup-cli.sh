47e231d7796a37682f7b5c9d111c0a96fa1130d2
Ferhat Yildiz <ferhat.yildiz@turingts.com>
2019-04-17 11:44:34 +0200

    install the latest version of Ansible using pip3

--- scripts/centos/setup-cli.sh
+++ scripts/centos/setup-cli.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 yum -y install git
-yum -y install ansible
+yum install -y https://centos7.iuscommunity.org/ius-release.rpm
+yum update -y
+yum install -y python36u python36u-libs python36u-devel python36u-pip
+pip3.6 install ansible
 
 cd /home/ansible/terraform-azure-playground/ansible && \
 export GIT_SSL_NO_VERIFY=true && \
